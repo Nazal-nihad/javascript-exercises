c5368601b7ddc5dc6bd1f0b312a1f09de573d37f
Nazal-nihad <nazalnihad17@gmail.com>
2023-05-20 21:38:38 +0530

    all done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  arr = people.reduce((oldest, currAge) => {
+    const oldest_man = age(oldest.yearOfBirth, oldest.yearOfDeath);
+    const current_man = age(currAge.yearOfBirth, currAge.yearOfDeath);
 
+    return oldest_man > current_man ? oldest : currAge;
+  });
+  return arr;
 };
 
+const age = (birth, death) => {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
 // Do not edit below this line
 module.exports = findTheOldest;
